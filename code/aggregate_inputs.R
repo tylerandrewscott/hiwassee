1e8169062372678f0d944b9edec29029fb7d0077
Tyler Scott <tascott@ucdavis.edu>
2022-05-04 13:55:00 -0700

    reaggregate at deal level

--- code/aggregate_inputs.R
+++ code/aggregate_inputs.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(stringr)
 library(tigris)
+library(tidyverse)
 counties <- tigris::list_counties('CA')
 # demographic measures
 d1 <- fread('input/prepped_county_input/county_demographs.txt')
@@ -23,6 +24,8 @@ d4$CFIPS <- as.character(d4$CFIPS)
 
 dt_all <- Reduce(function(x, y) merge(x, y,all = T), list(d1,d2,d3,d4))
 
+dt_all$CFIPS <- formatC(as.numeric(dt_all$CFIPS),width = 5,flag = '0')
+
 fwrite(dt_all,'input/Data_Clean_CountyRatings_V2.txt',sep='\t')
 
 
@@ -30,4 +33,3 @@ deals <- fread('input/prepped_county_input/CAdeal_ratings.csv')
 deals$CFIPS <- paste0('06',counties$county_code[match(deals$IssuerCounty,counties$county)])
 deals <- left_join(deals,dt_all)
 fwrite(deals,'input/Data_Clean_DealRatings_V2.txt',sep='\t')
-
