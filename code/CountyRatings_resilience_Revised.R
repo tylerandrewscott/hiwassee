b3ce4a5b2fe76fea36c7d58f468629421ef5c258
Tyler Scott <tascott@ucdavis.edu>
2022-06-27 16:08:04 -0700

    re-run models, generate fig2

--- code/CountyRatings_resilience_Revised.R
+++ code/CountyRatings_resilience_Revised.R
@@ -9,13 +9,13 @@ setnames(dt,'median_household_income','median_income')
 recode_ratings <- function(rating){
   rating[rating=='NR']<-NA
   rate = forcats::fct_collapse(rating,
-     '8' = c("AAA","Aaa"),
-     '7' = c("AA+","Aa1"),
-     '6' = c("AA","Aa2"),
-     '5' = c("AA-",'Aa3'),
-     '4' = c("A+",'A1'),
-     '3'= c("A","A2"),
-     '2' = c("A-","A3"),
+     '6' = c("AAA","Aaa"),#6
+     '5' = c("AA+","Aa1"),#5
+     '4' = c("AA","Aa2"),#4
+     '3' = c("AA-",'Aa3'),#3
+     '2' = c("A+",'A1'),
+     '1'= c("A","A2"),
+     '1' = c("A-","A3"),
     '1' = c("BBB+","Baa1","BBB","Baa2","BBB-","Baa3"))
   as.numeric(as.character(rate))
 }
@@ -34,19 +34,21 @@ library(MASS)
 dt$lntotalliabilities_generallongterm <- log(dt$totalliabilities_generallongterm+1)
 dt$transfers_total <- dt$totalstate + dt$totalfederal
 dt$lntransferstotal <- log(dt$transfers_total+1)
-dt$ln_ag_market_value <- log(dt$ag_market_value)
+dt$ln_median_home_value <- log(dt$median_home_value)
+dt$ln_totalliabilities_generallongterm_percapita <- log({dt$totalliabilities_generallongterm+1}/dt$population)
+
+summary(dt$totalliabilities_generallongterm)
 res_signals <- c('security','risk_management','preparedness','crisis_management')
 
 names(dt) <- gsub('\\s','_',names(dt))
 
 mdt <- dt[year %in% 2012:2017 & !is.na(crate) & !is.na(security),]
 
-#lntransferstotal + 
-summary(mdt$lntransferstotal/mdt$lnpop)
-linear_terms <- "scale(mrp_ideology_mean) + scale(ln_median_income) + scale(unemp_rate) + scale(lnpop) + scale(ln_ag_market_value) + scale(lntotalliabilities_generallongterm)" 
+linear_terms <- "scale(unemp_rate) + scale(lnpop) + scale(ln_median_home_value) + scale(ln_totalliabilities_generallongterm_percapita)" 
 
 mdt$cfips <- as.character(mdt$cfips)
 mdt$year <- as.character(mdt$year)
+
 
 library(lme4)
 library(INLA)
@@ -55,18 +57,25 @@ mdt$crate <- as.factor(mdt$crate)
 mdt$year <- as.factor(mdt$year)
 mdt$cfips <- as.factor(mdt$cfips)
 
+g1 <- ggplot(data = mdt) + geom_bar(aes(x = crate)) + theme_bw() + 
+  scale_y_continuous(name = '# county-year rating observations')+
+  ggtitle('Distribution of credit ratings by county and year in sample')+
+  scale_x_discrete(name = 'Highest rating',
+                   labels = rev(c('AAA','AA+','AA','AA-','A+','<=A'))) 
+saveRDS(object = g1,
+   file = 'scratch/panel1_figure2.rds')
+
 model_equations <- sapply(res_signals, function(x) 
   paste0("crate ~ 1 + year +", linear_terms,"+ scale(",x,")"))
 
 forms <- lapply(model_equations,as.formula)
 
-mod_list <- lapply(forms,function(i) 
-  clmm2(location = i,control = clmm2.control(maxIter = 1000),
-        random = cfips,data = mdt, link = 'logistic',nAGQ = 3,Hess = T))
-
+mod_list <- foreach(i = forms) %do% {
+  clmm2(location = i,control = clmm2.control(maxIter = 10e3),
+        random = cfips,data = mdt, trace = 0,link = 'logistic',nAGQ = 5,Hess = T)}
 
 coef_list <- mod_list %>% lapply(.,summary) %>% lapply(.,'[[','coefficients')
-
+names(coef_list) <- res_signals
 coef_dt <- rbindlist(lapply(seq_along(coef_list),function(x) 
   data.table(coef_list[[x]],Resilience = names(coef_list)[x],
              Coef = rownames(coef_list[[x]]))))
@@ -102,7 +111,17 @@ library(htmlTable)
 
 htmlTable(rbindlist(list(linear_coefs,add_terms,gof,nobs),use.name = F,fill = T),
           n.rgroup = c(nrow(linear_coefs),nrow(add_terms),nrow(gof),nrow(nobs)),
-          rgroup = c('linear predictors','additional model terms','goodness-of-fit','observations'),
+          rgroup = c('linear predictors (estimate + p-value)','additional model terms','goodness-of-fit','observations'),
            rnames = F ,file = 'output/tables/table3.html',
           caption = 'Table 3. Ordered Logistic Models for the Covariates of Underlying County Credit Ratings.')
+
+ggplot(data = mdt) + 
+  geom_point(aes(x = mrp_ideology_mean,y = median_home_value,col = crate)) + 
+  scale_color_viridis_d(name = 'rating') + ggtitle('County credit rating')
+
+ggplot(data = mdt) + geom_boxplot(aes(y = mrp_ideology_mean>0,
+                                      x = median_home_value)) + 
+  scale_y_discrete(name = 'ideology',labels = c('liberal','conservative'))
+
+
 
--- code/aggregate_inputs.R
+++ code/aggregate_inputs.R
@@ -7,6 +7,7 @@ counties <- tigris::list_counties('CA')
 # demographic measures
 d1 <- fread('input/prepped_county_input/county_demographs.txt')
 d1$CFIPS <- as.character(d1$CFIPS)
+
 # concept measures
 d2 <- fread('input/prepped_county_input/concept_results.txt')
 
@@ -20,18 +21,18 @@ d3[,County:=NULL]
 
 # financial data
 d4 <- fread('input/prepped_county_input/Combined_DIVER_Data.csv')
-d4[,County:=NULL]
-d4$CFIPS <- as.character(d4$CFIPS)
+d4$CFIPS <- paste0('6',counties$county_code[match(d4$me,counties$county)])
 
 dt_all <- Reduce(function(x, y) merge(x, y,all = T), list(d1,d2,d3,d4))
 
 dt_all$CFIPS <- formatC(as.numeric(dt_all$CFIPS),width = 5,flag = '0')
 
-
 fwrite(dt_all,'input/Data_Clean_CountyRatings_V2.txt',sep='\t')
-
 
 deals <- fread('input/prepped_county_input/CAdeal_ratings.csv')
 deals$CFIPS <- paste0('06',counties$county_code[match(deals$IssuerCounty,counties$county)])
+
 deals <- left_join(deals,dt_all)
+
+
 fwrite(deals,'input/Data_Clean_DealRatings_V2.txt',sep='\t')
